800cb1bb42360a391d360f813e27101a5ac4ac2e
Bianca <bianca92460877@gmail.com>
2023-09-21 20:17:06 -0300

    atualização correta

--- Protocol_TBL.java
+++ Protocol_TBL.java
@@ -1,37 +1,58 @@
+import java.util.Arrays;
 public class Protocol_TBL {
-    private char sync;
+    private byte sync = 0x54;
+    private long source = 51;
+    private long destination;
+    private byte[] protocolo;
+    private byte crc = 0;
 
-    private final int source = 57;
 
-    private int destination;
+    public byte getSync() {
 
-    private short size;
-
-    public char getSync() {
         return sync;
     }
 
-    public void setSync(char sync) {
-        this.sync = sync;
-    }
-
-    public int getSource() {
-        return source;
-    }
-
-    public int getDestination() {
-        return destination;
-    }
-
-    public void setDestination(int destination) {
+    public Protocol_TBL(long source, long destination) {
+        this.source = source;
         this.destination = destination;
     }
 
-    public short getSize() {
-        return size;
-    }
+    public byte[] encapsulation ( byte[] payload){
+        int len = 10;
+        int size = payload.length;
 
-    public void setSize(short size) {
-        this.size = size;
+        protocolo = new byte[size + 11];
+
+            protocolo[0] = sync;
+            protocolo[1] = (byte) (source & 0xff);
+            protocolo[2] = (byte) ((source >> 8) & 0xff);
+            protocolo[3] = (byte) ((source >> 16) & 0xff);
+            protocolo[4] = (byte) ((source >> 24) & 0xff);
+            protocolo[5] = (byte) (destination & 0xff);
+            protocolo[6] = (byte) ((destination >> 8) & 0xff);
+            protocolo[7] = (byte) ((destination >> 16) & 0xff);
+            protocolo[8] = (byte) ((destination >> 24) & 0xff);
+            protocolo[9] = (byte) (size & 0xff);
+            protocolo[10] = (byte) ((size >> 8) & 0xff);
+
+
+            for (int i = 0; i < size; i++) {
+                protocolo[len++] = payload[i];
+            }
+
+
+            CRC8 crcCalculator = new CRC8();
+            crcCalculator.update(protocolo);
+            crc = (byte) crcCalculator.getValue();
+
+            protocolo[len] = crc;
+            return protocolo;
+        }
+        public String toString() {
+            return "Sync: " + sync +
+                    "\nSource: " + source +
+                    "\ndestination: " + destination +
+                    "\nCRC: " + crc +
+                    "\nProtocolo: " + Arrays.toString(protocolo);
+        }
     }
-}
