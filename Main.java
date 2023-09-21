7b5366cdc7545ea6deb83bad62ceed1d721c9614
Biancakvieira <112039833+Biancakvieira@users.noreply.github.com>
2023-09-21 19:50:39 -0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,5 +1,28 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Protocolo protocolo = new Protocolo(666L, 777L);
+
+
+        String mensagem = "ola mundo";
+        byte array[] = mensagem.getBytes();
+        System.out.println(array.length);
+        byte protocoloTbl[] = protocolo.encapsularPacote(array);
+
+        System.out.println(protocoloTbl);
+
+        System.out.println(protocoloTbl.toString());
+
+
+
+//            CRC8 crc = new CRC8();
+//            crc.reset();
+//            crc.update("test".getBytes());
+//            System.out.println("181=" + crc.getValue());
+//            crc.reset();
+//            crc.update("hello world".getBytes());
+//            System.out.println("59=" + crc.getValue());
+//
+
     }
 }
